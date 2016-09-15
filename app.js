925ebfc5ac9720254bdea2cc30c7cccabd4477b7
Andy Evans <andrewevans0@outlook.com>
2016-09-15 08:30:21 +0100

    angular initalized and angular routing

---
+++ app.js
@@ -0,0 +1,10 @@
+angular
+    .module('app')
+    .controller('HomeController', HomeController);
+
+// HomeController.$inject = [];
+
+function HomeController() {
+    var vm = this;
+    vm.heading = "Apollo Web Designs";
+}
