94761ad5f9000dd4bb9c56e910eff1627c7ebf55
Andy Evans <andrewevans0@outlook.com>
2016-10-02 11:35:38 +0100

    changes

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-angular.module('app', []);
+angular.module('app', [['ngRoute']]);
 
 angular.module('app').controller('HomeController', HomeController);
 
 // HomeController.$inject = [];
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    templateUrl : "main.htm"
+  })
+  .when("/red", {
+    templateUrl : "red.htm"
+  })
+  .when("/green", {
+    templateUrl : "green.htm"
+  })
+  .when("/blue", {
+    templateUrl : "blue.htm"
+  });
+});
+
 
 function HomeController() {
     var vm = this;
