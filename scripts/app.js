af7d8fb2fec8195f376cba644d96358cd24e5731
Andy Evans <andrewevans0@outlook.com>
2017-04-22 22:03:49 +0100

    scripts and map added

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,23 @@
  */
 angular
     .module('app', []);
+
+
+angular
+    .module('app')
+    .config(function($stateProvider) {
+  var helloState = {
+    name: 'hello',
+    url: '/hello',
+    template: '<h3>hello world!</h3>'
+  }
+
+  var mapState = {
+    name: 'map',
+    url: '/map',
+    templateUrl: ''
+  }
+
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+});
