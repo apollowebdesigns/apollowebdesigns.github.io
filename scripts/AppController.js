983b724ae8300780ea5080d874fe456ce2d36e38
Andy Evans <andrewevans0@outlook.com>
2017-04-25 21:49:35 +0100

    callbacks added

--- scripts/AppController.js
+++ scripts/AppController.js
@@ -6,13 +6,33 @@ AppController.$inject = ['$http', '$log', 'ledService', 'driveService'];
 
 function AppController($http, $log, ledService, driveService) {
     var vm = this;
+
+    /* get callbacks */
+    getCallback ()
+        .then(getSecondCallback()) 
+        .then(console.log(vm.data));
+    
+
     vm.requestedData = 'test data';
     vm.getData = ledService.getData;
     vm.driveForwards = driveService.driveForwards;
     vm.driveBackwards = driveService.driveBackwards;
     vm.driveData = driveService.driveData;
     vm.driveData = driveService.driveData;
-    getCallback (); 
+    
+    function activateLoop() {
+        console.log(vm.data);
+        for (var i = 0; i < vm.data.length; i++) {
+            for (var j = 0; j < vm.data2.length; j++) {
+                if (i === j) {
+                    $log.info("new result");
+                    $log.info(vm.data[i] + " and this one " + vm.data[j]);
+                }
+            }
+        }
+    }
+
+    
 
 
     //http test
@@ -27,6 +47,22 @@ function AppController($http, $log, ledService, driveService) {
             $log.info("json response is");
             $log.info(response.data);
             vm.data = response.data;
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    }
+
+    function getSecondCallback () {
+        return $http({
+            method: 'GET',
+            url: 'test2.json'
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            $log.info("json response is");
+            $log.info(response.data);
+            vm.data2 = response.data;
         }, function errorCallback(response) {
             // called asynchronously if an error occurs
             // or server returns response with an error status.
